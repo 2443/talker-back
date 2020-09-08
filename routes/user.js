47ed03ac1d71cefb842488dcb0716cc0d7e42196
2443 <condol1209@gmail.com>
2020-09-08 22:54:41 +0900

    implement add friend

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 var express = require('express');
-const { User } = require('../models');
+const { User, Friend } = require('../models');
 var router = express.Router();
 const bcrypt = require('bcrypt');
+const { Op } = require('sequelize');
 
 router.post('/', async (req, res, next) => {
   try {
@@ -33,13 +34,100 @@ router.post('/', async (req, res, next) => {
 
 router.get('/', async (req, res, next) => {
   try {
-    const fullUserWithoutPassword = await User.findOne({
-      where: { id: req.user.id },
-      attributes: {
-        exclude: ['password'],
-      },
+    if (req.user) {
+      const fullUserWithoutPassword = await User.findOne({
+        where: { id: req.user.id },
+        attributes: {
+          exclude: ['password'],
+        },
+      });
+      return res.status(200).json(fullUserWithoutPassword);
+    }
+    return res.status(200).json(null);
+
+    // return res.json(user.toJson());
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get('/:key', async (req, res, next) => {
+  try {
+    const { key } = req.params;
+    const user = await User.findOne({
+      where: { [Op.or]: [{ id: key }, { email: key }] },
+      attributes: { exclude: ['password'] },
     });
-    return res.status(200).json(fullUserWithoutPassword);
+    return res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+// 친구 요청
+router.post('/friend/:id', async (req, res, next) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    await Friend.create({
+      UserId: req.user.id,
+      FriendId: id,
+      state: 'send',
+    });
+    await Friend.create({
+      UserId: id,
+      FriendId: req.user.id,
+      state: 'receive',
+    });
+
+    return res.status(200).json({ id });
+
+    // return res.json(user.toJson());
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.patch('/friend/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { state } = req.body;
+
+    console.log(id, state);
+
+    const where = {
+      [Op.or]: [
+        { UserId: req.user.id, FriendId: id },
+        { UserId: id, FriendId: req.user.id },
+      ],
+    };
+
+    if (state === 'accept') {
+      await Friend.update(
+        { state: 'friend' },
+        {
+          where,
+        }
+      );
+    } else if (state === 'reject') {
+      await Friend.destroy({
+        where,
+      });
+    } else if (state === 'block') {
+      await Friend.update(
+        {
+          state: 'block',
+        },
+        {
+          where,
+        }
+      );
+    }
+
+    return res.status(200).json({ id, state });
+
     // return res.json(user.toJson());
   } catch (error) {
     console.error(error);
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
+const { User, Friend } = require('../models');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+  const usersStates = {
+    send: [],
+    receive: [],
+    friend: [],
+    block: [],
+  };
+  try {
+    const users = await Friend.findAll({
+      where: { UserId: req.user.id },
+      // order: [
+      //   [User, 'nickname', 'DESC']
+      // ],
+      // where: { UserId: req.user.id },
+      include: [{ model: User, attributes: { exclude: ['password', 'email'] } }],
+    });
+
+    const sortedUsers = users.sort((a, b) => {
+      if (a.User.nickname < b.User.nickname) {
+        return -1;
+      }
+      if (a.User.nickname > b.User.nickname) {
+        return 1;
+      }
+    });
+
+    sortedUsers.forEach((e) => {
+      usersStates[e.state].push(e.User);
+    });
+
+    const mergedUsers = sortedUsers.map((e) => ({ ...e.User.dataValues, state: e.state }));
+
+    return res.status(200).json(mergedUsers);
+  } catch (error) {
+    return res.status(200).json([]);
+  }
 });
 
 module.exports = router;
